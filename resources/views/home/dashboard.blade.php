0454559508a7534ad6af0a72da46e33e88a304f8
DESKTOP-TQDUFB7\admin <109503655+deepredinktechdepartment@users.noreply.github.com>
2025-12-19 15:41:12 +0530

    changes on dashboard cards

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -13,40 +13,55 @@
 <div class="t-job-sheet container-fluid g-0">
     <div class="pt-3">
         <div class="row align-items-stretch">
-          <x-dashboard-card
-                title="Today's Appointments"
-                :count="$appointments_count"
-                route="{{ route('admin.appointments.report') }}"
-            />
-            <x-dashboard-card
-                title="Today's Payments"
-                :count="'₹ ' . number_format($today_collection, 2)"
-                route="{{ route('admin.payment.report') }}"
-            />
-            <!-- <x-dashboard-card
-                title="Specializations"
-                :count="$departments_count"
-                route="{{ route('admin.specializations') }}"
-            /> -->
 
+            {{-- APPOINTMENTS --}}
+            <x-card-today-month
+                title="Appointments"
+                :today="$appointments['today']"
+                :month="$appointments['month']"
+                :todayRoute="route('admin.appointments.report', [
+                    'from_date' => $today->toDateString(),
+                    'to_date' => $today->toDateString()
+                ])"
+                :monthRoute="route('admin.appointments.report', [
+                    'from_date' => $monthStart->toDateString(),
+                    'to_date' => $monthEnd->toDateString()
+                ])"
+            />
+
+            {{-- PAYMENTS --}}
+            <x-card-today-month
+                title="Payments"
+                :today="'₹ '.number_format($payments['today'], 2)"
+                :month="'₹ '.number_format($payments['month'], 2)"
+                :todayRoute="route('admin.payment.report', [
+                    'from_date' => $today->toDateString(),
+                    'to_date' => $today->toDateString()
+                ])"
+                :monthRoute="route('admin.payment.report', [
+                    'from_date' => $monthStart->toDateString(),
+                    'to_date' => $monthEnd->toDateString()
+                ])"
+            />
+
+            {{-- DOCTORS --}}
             <x-dashboard-card
                 title="Doctors"
                 :count="$doctors_count"
                 route="{{ route('admin.doctors') }}"
             />
 
+            {{-- PATIENTS --}}
             <x-dashboard-card
                 title="Patients"
                 :count="$patients_count"
                 route="{{ route('patients.index') }}"
             />
 
-
-
-
-
         </div>
     </div>
 </div>
+
+
 
 @endsection
