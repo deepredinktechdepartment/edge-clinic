db09c96123f9980f9fd5f2ba4f6b94c2e113dfa5
Jayaraju Vangapandu <109503655+deepredinktechdepartment@users.noreply.github.com>
2026-07-28 10:10:24 +0530

    Update bookapp.blade.php

--- resources/views/layouts/bookapp.blade.php
+++ resources/views/layouts/bookapp.blade.php
@@ -22,7 +22,7 @@
         <div class="container">
             <div class="row align-items-sm-center">
                 <div class="col-sm-2 col-5">
-                    <a href="https://edge.clinic"><img src="https://edge.clinic/for-doctors/wp-content/uploads/2025/06/edge_logo.png" alt="Edge Clinic Logo" class="img-fluid" width="130"></a>
+                    <a href="https://edge.clinic"><img src="https://edge.clinic/wp-content/uploads/2025/06/edge_logo.png" alt="Edge Clinic Logo" class="img-fluid" width="130"></a>
                 </div>
                 <div class="col-sm-9 col-5">
                     <nav class="navbar navbar-expand-lg p-0">
@@ -39,7 +39,7 @@
 
 <li class="nav-item">
     <a class="nav-link"
-       href="https://edge.clinic/for-doctors">
+       href="https://edge.clinic/">
         For Doctors
     </a>
 </li>
@@ -69,7 +69,7 @@
                 </div>
                 <div class="col-sm-1 col-2 px-sm-3 px-0">
                     <div class="whatsappicon-wrapper">
-                        <a href="https://api.whatsapp.com/send?phone=916303285050" target="_blank"><img src="{{ asset('assets/img/WhatsApp.png')}}" alt="whatsapp" class="img-fluid"></a>
+                        <a href="https://api.whatsapp.com/send?phone=919392585050" target="_blank"><img src="{{ asset('assets/img/WhatsApp.png')}}" alt="whatsapp" class="img-fluid"></a>
                     </div>
                 </div>
             </div>
@@ -83,12 +83,12 @@
         <div class="row">
             <div class="col-sm-6">
                 <div class="mb-4">
-                    <img src="https://edge.clinic/for-doctors/wp-content/uploads/2025/06/edge_logo.png" alt="Edge Clinic" class="img-fluid" width="150">
+                    <img src="https://edge.clinic/wp-content/uploads/2025/06/edge_logo.png" alt="Edge Clinic" class="img-fluid" width="150">
                 </div>
                 <div>
                     <h4>Connect with Us</h4>
                     <ul class="mt-3 mb-3">
-                        <li><a href="tel:+91 6303285050"><i class="fa-solid fa-phone"></i> &nbsp; &nbsp; +91 6303285050</a></li>
+                        <li><a href="tel:+91 9392585050"><i class="fa-solid fa-phone"></i> &nbsp; &nbsp; +91 9392585050</a></li>
                         <li><a href="tel:+91 9908085050"><i class="fa-brands fa-whatsapp"></i> &nbsp; &nbsp; +91 9908085050</a></li>
                     </ul>
                     <p><b>Hyderabad</b> | Pune* | Bengaluru* *Upcoming</p>
